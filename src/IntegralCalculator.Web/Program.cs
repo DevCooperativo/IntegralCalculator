70fb7d69a267c559b9c42fd41a3901454ad55105
LFerriVM <lucasfvm60@gmail.com>
2023-11-26 00:33:26 -0300

    implementacao basica da index

--- src/IntegralCalculator.Web/Program.cs
+++ src/IntegralCalculator.Web/Program.cs
@@ -17,8 +17,8 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-app.UseHttpsRedirection();
 app.UseStaticFiles();
+app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
